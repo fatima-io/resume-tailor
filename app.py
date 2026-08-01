df2e9f1ed2d904cc45154a770515c886cc89e36c
fatima-io <faatiimaakhtar@gmail.com>
2026-08-01 23:53:06 +0500

    another import

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from utils.file_parser import parse_uploaded_file
 
 app = Flask(__name__)
 
@@ -17,9 +18,16 @@ def generate():
 
     resume_file = request.files.get("resume_file")
 
+    resume_text = ""
+
+    if resume_file and resume_file.filename != "":
+        resume_text = parse_uploaded_file(resume_file)
+
+    print("========== JOB DESCRIPTION ==========")
     print(job_description)
-    print(job_file)
-    print(resume_file)
+
+    print("========== RESUME ==========")
+    print(resume_text)
 
     return "Files received successfully!"
 
